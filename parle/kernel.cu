aac2630a70aff2980a603deded30c81f322cbcaf
Erkaman <arnebackeric@gmail.com>
2016-06-24 18:52:53 +0200

    cleanup

--- parle/kernel.cu
+++ parle/kernel.cu
@@ -67,58 +67,17 @@ CUDPPHandle cudpp;
 void parleDevice(int *d_in, int n,
 	int* d_symbolsOut,
 	int* d_countsOut,
-	int* d_totalRuns,
-	bool useChag
+	int* d_totalRuns
 	);
 
 
 int parleHost(int *h_in, int n,
 	int* h_symbolsOut,
-	int* h_countsOut, bool useChag);
+	int* h_countsOut);
 
 int parleCpu(int *in, int n,
 	int* symbolsOut,
 	int* countsOut);
-
-/*
-__global__ void maskKernel(int *g_in, int* g_backwardMask, int* g_forwardMask, int n) {
-
-	extern __shared__ int shared[];
-
-	const int RADIUS = 1;
-	const int BLOCK_SIZE = blockDim.x;
-
-	for (int gindex : hemi::grid_stride_range(0, n)) {
-
-		int lindex = threadIdx.x + RADIUS;
-
-		shared[lindex] = g_in[gindex];
-
-		if (threadIdx.x < RADIUS) {
-			shared[lindex - RADIUS] = g_in[gindex - RADIUS];
-			shared[lindex + BLOCK_SIZE] = g_in[gindex +BLOCK_SIZE];
-		}
-
-		__syncthreads();
-
-		if (gindex == 0)
-			g_backwardMask[gindex] = 1;
-		else {
-			// shared[lindex - 1]
-			g_backwardMask[gindex] = (shared[lindex] != shared[lindex - 1]);
-		}
-
-		if (gindex == (n - 1))
-			g_forwardMask[gindex] = 1;
-		else {
-			g_forwardMask[gindex] = (shared[lindex] !=	shared[lindex + 1]);
-		}
-		
-		
-	}
-}
-
-*/
 
 __global__ void compactKernel(int* g_in, int* g_scannedBackwardMask, int* g_compactedBackwardMask, int* g_totalRuns, int n) {
 
@@ -164,42 +123,6 @@ __global__ void scatterKernel(int* g_compactedBackwardMask, int* g_totalRuns, in
 
 }
 
-
-__global__ void maskKernel(int *g_in, int* g_backwardMask, int n) {
-
-	extern __shared__ int shared[];
-
-	const int RADIUS = 1;
-	const int BLOCK_SIZE = blockDim.x;
-
-	for (int gindex : hemi::grid_stride_range(0, n)) {
-
-		int lindex = threadIdx.x + RADIUS;
-
-		shared[lindex] = g_in[gindex];
-
-		if (threadIdx.x < RADIUS) {
-			shared[lindex - RADIUS] = g_in[gindex - RADIUS];
-		}
-
-		__syncthreads();
-
-		if (gindex == 0) {
-			g_backwardMask[gindex] = 1;
-		} else {
-
-			int a = shared[lindex];
-			int b = shared[lindex-1];
-
-			g_backwardMask[gindex] = (a != b);
-		}
-
-	}
-}
-
-
-
-/*
 __global__ void maskKernel(int *g_in, int* g_backwardMask, int n) {
 
 	for (int i : hemi::grid_stride_range(0, n)) {
@@ -211,14 +134,9 @@ __global__ void maskKernel(int *g_in, int* g_backwardMask, int n) {
 	}
 }
 
-*/
-
 void PrintArray(int* arr, int n){
 	for (int i = 0; i < n; ++i){
 		printf("%d, ", arr[i]);
-		/*if ( (i+1) % j == (0) && i!=0 ){
-			printf("| ");
-			}*/
 	}
 	printf("\n");
 }
@@ -228,9 +146,6 @@ char errorString[256];
 bool verifyCompression(
 	int* original, int n,
 	int* compressedSymbols, int* compressedCounts, int totalRuns){
-
-	
-
 
 	// decompress.
 	int j = 0;
@@ -253,7 +168,6 @@ bool verifyCompression(
 		return false;
 	}
 
-
 	for (int i = 0; i < totalRuns; ++i){
 		int symbol = compressedSymbols[i];
 		int count = compressedCounts[i];
@@ -272,20 +186,11 @@ bool verifyCompression(
 		}
 	}
 
-
 	return true;
-
-	//printf("Decompressed:     ");
-	//	PrintArray(decompressed, n);
-
-
-
-
 }
 
+// get random test data for compression.
 int* getRandomData(int n){
-
-
 	int val = rand() % 10;
 
 	for (int i = 0; i < n; ++i) {
@@ -299,12 +204,10 @@ int* getRandomData(int n){
 	return g_in;
 }
 
-
+// use F to RLE compresss the data, and then verify the compression. 
 template<typename F>
 void unitTest(int* in, int n, F rle, bool verbose)
 {
-
-	
 	int totalRuns = rle(in, n, g_symbolsOut, g_countsOut);
 		//parleHost(in, n, symbolsOut, countsOut); // 1<<8
 
@@ -326,103 +229,64 @@ void unitTest(int* in, int n, F rle, bool verbose)
 		if (verbose)
 			printf("passed test!\n\n");
 	}
-
 }
-
 
 template<typename F>
 void profileCpu(F rle) {
-
 	for (int i = 0; i < NUM_TESTS; ++i) {
-
-
 		int n = Tests[i];
-		
 		int* in = getRandomData(n);
 		
-
 		for (int i = 0; i < TRIALS; ++i) {	
-
 			sdkStartTimer(&timer);
-
 			rle(in, n, g_symbolsOut, g_countsOut);
-
 			sdkStopTimer(&timer);
-
 		}
 
 		// also unit test, to make sure that the compression is valid.
 		unitTest(in, n, rle, false);
 
-
 		printf("For n = %d, in time %.5f\n", n, sdkGetAverageTimerValue(&timer)*1e-3);
-		
-
-
 	}
-
 }
 
 template<typename F>
-void profileGpu(bool useChag, F f) {
+void profileGpu(F f) {
 
 	cudaEvent_t start, stop;
-
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
 
-
 	for (int i = 0; i < NUM_TESTS; ++i) {
 
-
 		int n = Tests[i];
-
 		int* in = getRandomData(n);
-		
-
-
-
-		
 		int h_totalRuns;
-
-
 
 		// transer input data to device.
 		CUDA_CHECK(cudaMemcpy(d_in, in, n*sizeof(int), cudaMemcpyHostToDevice));
 
+		// record.
 		cudaEventRecord(start);
-
 		for (int i = 0; i < TRIALS; ++i) {
-
-			// RUN.
-			parleDevice(d_in, n, d_symbolsOut, d_countsOut, d_totalRuns, useChag);
-
-			
+			parleDevice(d_in, n, d_symbolsOut, d_countsOut, d_totalRuns);
 		}
-
-
 		cudaEventRecord(stop);
 		cudaDeviceSynchronize();
 
-		
 		// also unit test, to make sure that the compression is valid.
 		unitTest(in, n, f, false);
 
 		float ms;
 		cudaEventElapsedTime(&ms, start, stop);
 		printf("For n = %d, in time %.5f\n", n, (ms/((float)TRIALS ) ) /1000.0f);
-
-
 	}
-
 }
-
 
 template<typename F>
 void runTests(int a, F rle) {
-	printf("START\n");
+	printf("START UNIT TESTS\n");
 
-	
 	for (int i = 4; i < a; ++i) {
 
 		for (int k = 0; k < 30; ++k) {
@@ -438,20 +302,17 @@ void runTests(int a, F rle) {
 			int* in = getRandomData(n);
 
 			unitTest(in, n, rle, true);
-
 		}
 
 		printf("-------------------------------\n\n");
 	}
 }
 
-int main()
-{
+int main(){
 
 	sdkCreateTimer(&timer);
 	srand(1000);
 	CUDA_CHECK(cudaSetDevice(0));
-
 
 	cudppCreate(&cudpp);
 	//
@@ -462,45 +323,33 @@ int main()
 	config.datatype = CUDPP_INT;
 	config.algorithm = CUDPP_SCAN;
 	config.options = CUDPP_OPTION_FORWARD | CUDPP_OPTION_INCLUSIVE;
-
 	CUDPPResult res = cudppPlan(cudpp, &scanPlan, config, MAX_N, 1, 0);
-
 	if (CUDPP_SUCCESS != res){
 		printf("Error creating CUDPPPlan for scan2!\n");
 		exit(-1);
 	}
-
 
 	// allocate resources on device. These arrays are used globally thoughouts the program.
 	CUDA_CHECK(cudaMalloc((void**)&d_backwardMask, MAX_N * sizeof(int)));
 	CUDA_CHECK(cudaMalloc((void**)&d_scannedBackwardMask, MAX_N * sizeof(int)));
 	CUDA_CHECK(cudaMalloc((void**)&d_compactedBackwardMask, (MAX_N+ 1) * sizeof(int)));
 
-	// MALLOC
 	CUDA_CHECK(cudaMalloc((void**)&d_in, MAX_N* sizeof(int)));
 	CUDA_CHECK(cudaMalloc((void**)&d_countsOut, MAX_N * sizeof(int)));
 	CUDA_CHECK(cudaMalloc((void**)&d_symbolsOut, MAX_N * sizeof(int)));
 	CUDA_CHECK(cudaMalloc((void**)&d_totalRuns, sizeof(int)));
 
-
+	// allocate resources on the host. 
 	g_in = new int[MAX_N];
 	g_decompressed = new int[MAX_N];
 
 	g_symbolsOut = new int[MAX_N];
 	g_countsOut = new int[MAX_N];
 
-
-
-	auto rleGpuCudpp = [](int *in, int n,
+	auto rleGpu = [](int *in, int n,
 		int* symbolsOut,
 		int* countsOut){
-		return parleHost(in, n, symbolsOut, countsOut, false);
-	};
-
-	auto rleGpuChag = [](int *in, int n,
-		int* symbolsOut,
-		int* countsOut){
-		return parleHost(in, n, symbolsOut, countsOut, true);
+		return parleHost(in, n, symbolsOut, countsOut);
 	};
 	
 	auto rleCpu = [](int *in, int n,
@@ -509,50 +358,17 @@ int main()
 		return parleCpu(in, n, symbolsOut, countsOut);
 	};
 
-//	auto rle = rleGpu;
-
-	
-	
-	
-	/*
-	int n = 8;
-	int* in = new int[n]
-	{
-	//	1,2,3,4,4,4,2,2
-
-	//	1,1,2, 4,4,4,4,4
-
-		5,5,5,5,5,   1,2,3
-	}
-	;
-
-	unitTest(in, n, rleGpuChag, true); // 35
-	delete[]in;
-	*/
-	
-
-	
 
 	// uni tests
-	//runTests(21, rleGpuChag);
-
-	//runTests(21, rleGpuCudpp);
-	
+	//runTests(21, rleGpu);
 	//runTests(21, rleCpu);
 
-	
+	printf("profile CPU\n");
+	profileCpu(rleCpu);
 
-	
-	
-	//printf("For CPU\n");
-	//profileCpu(rleCpu);
+	printf("profile GPU\n");
+	profileCpu(rleGpu);
 
-	//rintf("For GPU\n");
-	//profileCpu(rleGpu);
-
-
-
-	
 	//printf("For GPU CHAG\n");
 	//profileGpu(true, rleGpuChag);
 	
@@ -562,34 +378,22 @@ int main()
 
 
 	//Visual Prof
-	
-	
-
-	
 	int n = 1 << 23;
 	// also unit test, to make sure that the compression is valid.
-	unitTest(getRandomData(1<<23), n, rleGpuChag, true);
-
-
-
+	unitTest(getRandomData(1<<23), n, rleGpu, true);
 
 	// free device arrays.
 	CUDA_CHECK(cudaFree(d_backwardMask));
 	CUDA_CHECK(cudaFree(d_scannedBackwardMask));
 	CUDA_CHECK(cudaFree(d_compactedBackwardMask));
-
-	// FREE
 	CUDA_CHECK(cudaFree(d_in));
 	CUDA_CHECK(cudaFree(d_countsOut));
 	CUDA_CHECK(cudaFree(d_symbolsOut));
 	CUDA_CHECK(cudaFree(d_totalRuns));
 
-
-
-
+	// cudpp free.
 	res = cudppDestroyPlan(scanPlan);
-	if (CUDPP_SUCCESS != res)
-	{
+	if (CUDPP_SUCCESS != res){
 		printf("Error destroying CUDPPPlan for scan2\n");
 		exit(-1);
 	}
@@ -597,35 +401,22 @@ int main()
 
 	CUDA_CHECK(cudaDeviceReset());
 
-	// free global memory.
+	// free host memory.
 	delete[] g_in;
 	delete[] g_decompressed;
 
 	delete[] g_symbolsOut;
 	delete[] g_countsOut;
 
-
-
-	printf("DONE\n");
-
-
-
-
 	return 0;
 }
 
-void scan2(int* d_in, int* d_out, int N, bool useChag) {
-
-
-
+void scan2(int* d_in, int* d_out, int N) {
 	CUDPPResult res = cudppScan(scanPlan, d_out, d_in, N);
 		if (CUDPP_SUCCESS != res){
 			printf("Error in cudppScan2()\n");
 			exit(-1);
 		}
-
-
-
 }
 
 int parleCpu(int *in, int n,
@@ -634,7 +425,6 @@ int parleCpu(int *in, int n,
 
 	if (n == 0)
 		return 0; // nothing to compress!
-
 
 	int outIndex = 0;
 	int symbol = in[0];
@@ -658,7 +448,6 @@ int parleCpu(int *in, int n,
 			// run is not over yet.
 			++count;
 		}
-
 	}
 
 	if (count > 0) {
@@ -671,107 +460,36 @@ int parleCpu(int *in, int n,
 
 }
 
+// On the CPU do preparation to run parle, launch PARLE on GPU, and then transfer the result data to the CPU. 
 int parleHost(int *h_in, int n,
 	int* h_symbolsOut,
-	int* h_countsOut,
-	bool useChag){
+	int* h_countsOut){
 
 	int h_totalRuns;
 
-	
-	/*
-	printf("N: %d\n", n);
-	printf("n: %d\n", n);
-	
-	printf("orig:             ");
-	PrintArray(h_in, n);
-	*/
-
-	
 	// transer input data to device.
 	CUDA_CHECK(cudaMemcpy(d_in, h_in, n*sizeof(int), cudaMemcpyHostToDevice));
 
-
 	// RUN.	
-	parleDevice(d_in, n, d_symbolsOut, d_countsOut, d_totalRuns, useChag);
+	parleDevice(d_in, n, d_symbolsOut, d_countsOut, d_totalRuns);
 
-	// MEMCPY
+	// transer result data to host.
 	CUDA_CHECK(cudaMemcpy(h_symbolsOut, d_symbolsOut, n*sizeof(int), cudaMemcpyDeviceToHost));
 	CUDA_CHECK(cudaMemcpy(h_countsOut, d_countsOut, n*sizeof(int), cudaMemcpyDeviceToHost));
 	CUDA_CHECK(cudaMemcpy(&h_totalRuns, d_totalRuns, sizeof(int), cudaMemcpyDeviceToHost));
 
-	//printf("total runs: %d\n", h_totalRuns);
-
 	return h_totalRuns;
-
 }
 
-
+// run parle on the GPU
 void parleDevice(int *d_in, int n,
 	int* d_symbolsOut,
 	int* d_countsOut,
-	int* d_totalRuns, // keeps track of the total number of runs that the data was compressed down to.
-	bool useChag
+	int* d_totalRuns
 	){
 
-	
-	
-	
-	const int BLOCK_SIZE = 1024;
-	hemi::ExecutionPolicy ep;
-	ep.setBlockSize(BLOCK_SIZE); // TODO: compute this using occupancy API.
-	ep.setSharedMemBytes((BLOCK_SIZE + 1)*sizeof(int));
-	
-	hemi::cudaLaunch(ep, maskKernel, d_in, d_backwardMask, n);
-	
-	scan2(d_backwardMask, d_scannedBackwardMask, n, useChag);
-	
+	hemi::cudaLaunch(maskKernel, d_in, d_backwardMask, n);
+	scan2(d_backwardMask, d_scannedBackwardMask, n);
 	hemi::cudaLaunch(compactKernel, d_in, d_scannedBackwardMask, d_compactedBackwardMask, d_totalRuns, n);
-
-
-	
 	hemi::cudaLaunch(scatterKernel, d_compactedBackwardMask, d_totalRuns, d_in, d_symbolsOut, d_countsOut);
-	
-
-
-	/*
-	int* h_backwardMask = new int[n];
-	int* h_scannedBackwardMask = new int[n];
-	int* h_compactedBackwardMask = new int[n+1];
-	
-
-	CUDA_CHECK(cudaMemcpy(h_backwardMask, d_backwardMask, n*sizeof(int), cudaMemcpyDeviceToHost));
-	CUDA_CHECK(cudaMemcpy(h_scannedBackwardMask, d_scannedBackwardMask, n*sizeof(int), cudaMemcpyDeviceToHost));
-	
-	CUDA_CHECK(cudaMemcpy(h_compactedBackwardMask, d_compactedBackwardMask, (n+1)*sizeof(int), cudaMemcpyDeviceToHost));
-
-
-	printf("Backward:         ");
-	PrintArray(h_backwardMask, n);
-
-	printf("Scanned Backward: ");
-	PrintArray(h_scannedBackwardMask, n);
-
-	//printf("h_totalRuns:      %d\n", h_totalRuns);
-
-	//printf("h_symbolsOut:     ");
-	//PrintArray(h_symbolsOut, h_totalRuns);
-
-	//printf("h_countsOut:      ");
-	//PrintArray(h_countsOut, h_totalRuns);
-
-	printf("d_compactedBackwardMask:     ");
-	PrintArray(h_compactedBackwardMask, 10);
-
-
-	delete[] h_backwardMask;
-	delete[] h_scannedBackwardMask;
-	delete[] h_compactedBackwardMask;
-	*/
-
-	
-
-
 }
-
-//next thing to try: make sure that scatter and compact kernel does not launch that many threads!
